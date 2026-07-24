c03835c239fb94588d7c5e0e3026fd6ea4978b50
Bernat Mestres <ber.mestres@gmail.com>
2026-07-24 23:19:00 +0200

    fix: adjusted foreign keys in pizza_parlour

--- PizzaParlourDB/pizza_parlour.sql
+++ PizzaParlourDB/pizza_parlour.sql
@@ -12,7 +12,7 @@ DROP SCHEMA IF EXISTS `pizza_parlour` ;
 -- -----------------------------------------------------
 -- Schema pizza_parlour
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `pizza_parlour` DEFAULT CHARACTER SET UTF8MB4;
+CREATE SCHEMA IF NOT EXISTS `pizza_parlour` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `pizza_parlour` ;
 
 -- -----------------------------------------------------
@@ -22,8 +22,11 @@ CREATE TABLE IF NOT EXISTS `pizza_parlour`.`province` (
   `province_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(60) NOT NULL,
   PRIMARY KEY (`province_id`),
-  UNIQUE INDEX `province_id_UNIQUE` (`province_id` ASC))
-ENGINE = InnoDB;
+  UNIQUE INDEX `province_id_UNIQUE` (`province_id` ASC) VISIBLE)
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -34,13 +37,16 @@ CREATE TABLE IF NOT EXISTS `pizza_parlour`.`locality` (
   `name` VARCHAR(50) NOT NULL,
   `province_id` INT NOT NULL,
   PRIMARY KEY (`locality_id`),
-  INDEX `fk_locality_province1_idx` (`province_id` ASC),
+  INDEX `fk_locality_province1_idx` (`province_id` ASC) VISIBLE,
   CONSTRAINT `fk_locality_province1`
     FOREIGN KEY (`province_id`)
     REFERENCES `pizza_parlour`.`province` (`province_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -49,15 +55,15 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `pizza_parlour`.`client` (
   `client_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(60) NOT NULL,
-  `surname` VARCHAR(60) NULL,
+  `surname` VARCHAR(60) NULL DEFAULT NULL,
   `address` VARCHAR(60) NOT NULL,
   `locality_id` INT NOT NULL,
   `province_id` INT NOT NULL,
-  `phone` VARCHAR(20) NULL,
+  `phone` VARCHAR(20) NULL DEFAULT NULL,
   `postal_code` INT NOT NULL,
   PRIMARY KEY (`client_id`),
-  INDEX `fk_client_locality_idx` (`locality_id` ASC),
-  INDEX `fk_client_province1_idx` (`province_id` ASC),
+  INDEX `fk_client_locality_idx` (`locality_id` ASC) VISIBLE,
+  INDEX `fk_client_province1_idx` (`province_id` ASC) VISIBLE,
   CONSTRAINT `fk_client_locality`
     FOREIGN KEY (`locality_id`)
     REFERENCES `pizza_parlour`.`locality` (`locality_id`)
@@ -68,7 +74,10 @@ CREATE TABLE IF NOT EXISTS `pizza_parlour`.`client` (
     REFERENCES `pizza_parlour`.`province` (`province_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -81,8 +90,8 @@ CREATE TABLE IF NOT EXISTS `pizza_parlour`.`store` (
   `locality_id` INT NOT NULL,
   `province_id` INT NOT NULL,
   PRIMARY KEY (`store_id`),
-  INDEX `fk_store_locality1_idx` (`locality_id` ASC),
-  INDEX `fk_store_province1_idx` (`province_id` ASC),
+  INDEX `fk_store_locality1_idx` (`locality_id` ASC) VISIBLE,
+  INDEX `fk_store_province1_idx` (`province_id` ASC) VISIBLE,
   CONSTRAINT `fk_store_locality1`
     FOREIGN KEY (`locality_id`)
     REFERENCES `pizza_parlour`.`locality` (`locality_id`)
@@ -93,7 +102,10 @@ CREATE TABLE IF NOT EXISTS `pizza_parlour`.`store` (
     REFERENCES `pizza_parlour`.`province` (`province_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -104,17 +116,20 @@ CREATE TABLE IF NOT EXISTS `pizza_parlour`.`employee` (
   `name` VARCHAR(50) NOT NULL,
   `surname` VARCHAR(50) NOT NULL,
   `nif` VARCHAR(9) NOT NULL,
-  `phone` VARCHAR(15) NULL,
-  `role` ENUM('cook', 'rider') NULL,
+  `phone` VARCHAR(15) NULL DEFAULT NULL,
+  `role` ENUM('cook', 'rider') NULL DEFAULT NULL,
   `store_id` INT NOT NULL,
   PRIMARY KEY (`employee_id`),
-  INDEX `fk_employee_store1_idx` (`store_id` ASC),
+  INDEX `fk_employee_store1_idx` (`store_id` ASC) VISIBLE,
   CONSTRAINT `fk_employee_store1`
     FOREIGN KEY (`store_id`)
     REFERENCES `pizza_parlour`.`store` (`store_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -123,31 +138,34 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `pizza_parlour`.`order` (
   `order_id` INT NOT NULL AUTO_INCREMENT,
   `date_time` TIMESTAMP NOT NULL,
-  `delivery_type` ENUM('DELIVERY', 'PICKUP') NULL,
+  `delivery_type` ENUM('DELIVERY', 'PICKUP') NULL DEFAULT NULL,
   `client_id` INT NOT NULL,
   `store_id` INT NOT NULL,
   `employee_id` INT NOT NULL,
   PRIMARY KEY (`order_id`),
-  UNIQUE INDEX `order_id_UNIQUE` (`order_id` ASC),
-  INDEX `fk_order_client1_idx` (`client_id` ASC),
-  INDEX `fk_order_store1_idx` (`store_id` ASC),
-  INDEX `fk_order_employee1_idx` (`employee_id` ASC),
+  UNIQUE INDEX `order_id_UNIQUE` (`order_id` ASC) VISIBLE,
+  INDEX `fk_order_client1_idx` (`client_id` ASC) VISIBLE,
+  INDEX `fk_order_store1_idx` (`store_id` ASC) VISIBLE,
+  INDEX `fk_order_employee1_idx` (`employee_id` ASC) VISIBLE,
   CONSTRAINT `fk_order_client1`
     FOREIGN KEY (`client_id`)
     REFERENCES `pizza_parlour`.`client` (`client_id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `fk_order_store1`
-    FOREIGN KEY (`store_id`)
-    REFERENCES `pizza_parlour`.`store` (`store_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_order_employee1`
     FOREIGN KEY (`employee_id`)
     REFERENCES `pizza_parlour`.`employee` (`employee_id`)
     ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_order_store1`
+    FOREIGN KEY (`store_id`)
+    REFERENCES `pizza_parlour`.`store` (`store_id`)
+    ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -155,9 +173,12 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `pizza_parlour`.`product_category` (
   `product_category_id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(50) NULL,
+  `name` VARCHAR(50) NULL DEFAULT NULL,
   PRIMARY KEY (`product_category_id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -166,14 +187,14 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `pizza_parlour`.`product` (
   `product_id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(60) NOT NULL,
-  `description` VARCHAR(350) NULL,
-  `image` BLOB NULL,
-  `price` DECIMAL(4,2) NULL,
+  `description` VARCHAR(350) NULL DEFAULT NULL,
+  `image` BLOB NULL DEFAULT NULL,
+  `price` DECIMAL(4,2) NULL DEFAULT NULL,
   `order_id` INT NOT NULL,
   `product_category_id` INT NOT NULL,
   PRIMARY KEY (`product_id`),
-  INDEX `fk_product_order1_idx` (`order_id` ASC),
-  INDEX `fk_product_product_category1_idx` (`product_category_id` ASC),
+  INDEX `fk_product_order1_idx` (`order_id` ASC) VISIBLE,
+  INDEX `fk_product_product_category1_idx` (`product_category_id` ASC) VISIBLE,
   CONSTRAINT `fk_product_order1`
     FOREIGN KEY (`order_id`)
     REFERENCES `pizza_parlour`.`order` (`order_id`)
@@ -184,7 +205,9 @@ CREATE TABLE IF NOT EXISTS `pizza_parlour`.`product` (
     REFERENCES `pizza_parlour`.`product_category` (`product_category_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -195,13 +218,15 @@ CREATE TABLE IF NOT EXISTS `pizza_parlour`.`order_line` (
   `units` INT NOT NULL,
   `product_id` INT NOT NULL,
   PRIMARY KEY (`order_line_id`),
-  INDEX `fk_order_line_product1_idx` (`product_id` ASC),
+  INDEX `fk_order_line_product1_idx` (`product_id` ASC) VISIBLE,
   CONSTRAINT `fk_order_line_product1`
     FOREIGN KEY (`product_id`)
     REFERENCES `pizza_parlour`.`product` (`product_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -211,8 +236,8 @@ CREATE TABLE IF NOT EXISTS `pizza_parlour`.`order_has_order_line` (
   `order_id` INT NOT NULL,
   `order_line_id` INT NOT NULL,
   PRIMARY KEY (`order_id`, `order_line_id`),
-  INDEX `fk_order_has_order_line_order_line1_idx` (`order_line_id` ASC),
-  INDEX `fk_order_has_order_line_order1_idx` (`order_id` ASC),
+  INDEX `fk_order_has_order_line_order_line1_idx` (`order_line_id` ASC) VISIBLE,
+  INDEX `fk_order_has_order_line_order1_idx` (`order_id` ASC) VISIBLE,
   CONSTRAINT `fk_order_has_order_line_order1`
     FOREIGN KEY (`order_id`)
     REFERENCES `pizza_parlour`.`order` (`order_id`)
@@ -223,7 +248,9 @@ CREATE TABLE IF NOT EXISTS `pizza_parlour`.`order_has_order_line` (
     REFERENCES `pizza_parlour`.`order_line` (`order_line_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
